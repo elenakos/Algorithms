3fad6736e0f3c31d860c72ea67d1a944063397a1
elenakos <kostrygina@gmail.com>
2025-02-07 15:20:50 -0800

    Added Verify_string_anagram.py

---
+++ Lengh_of_longest_unique_substring.py
@@ -0,0 +1,47 @@
+'''
+Given a string s, find the length of the longest substring without repeating characters.
+
+
+Example:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+'''
+
+import unittest
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        print("\nFind the longest substring without repeating characters {}".format(s))
+
+        if len(s) == 0:
+            print("==> This string is empty")
+            return 0
+
+        allUniqueString = []
+        allLength = []
+        for i in range(len(s)):
+            currentUnigueSubstring = ""
+            for j in range( i, len(s)):
+                if s[j] not in currentUnigueSubstring:
+                    currentUnigueSubstring += s[j]
+                else:
+                    break
+                allUniqueString.append(currentUnigueSubstring)
+                allLength.append(len(currentUnigueSubstring))
+        print("Unique string: {}".format(allUniqueString))
+        return max(allLength)
+
+
+class TestSolution(unittest.TestCase):
+    def test_should_return_three(self):
+        stringToTest = "abcabcbb"
+        result = Solution().lengthOfLongestSubstring(stringToTest)
+        self.assertEqual(3, result)
+
+
+    def test_should_return_nine(self):
+        stringToTest = "1236547899787"
+        result = Solution().lengthOfLongestSubstring(stringToTest)
+        self.assertEqual(9, result)
