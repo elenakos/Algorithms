3c7baecd3882bff6777326a4360e40ce3c66dc97
elenakos <kostrygina@gmail.com>
2025-06-26 11:33:17 -0700

    Added test cases

--- Valid_parentheses_pairs.py
+++ Valid_parentheses_pairs.py
@@ -5,6 +5,10 @@ An input string is valid if:
 - Open brackets must be closed by the same type of brackets.
 - Open brackets must be closed in the correct order.
 - Every close bracket has a corresponding open bracket of the same type.
+
+There are two implementations:
+- the first defined  two lists for open/closed parenthesis
+- and the second is using a dictionary for open/closed parenthesis
 '''
 
 import unittest
